e978a26cd07a49c7e7683fcf22c8ffb0ffc018eb
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-23 15:55:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+width.innerText = window.outerWidth;
+height.innerText = window.outerHeight;
+
+window.addEventListener("resize", () => {
+	width.innerText = window.outerWidth;
+	height.innerText = window.outerHeight;
+})
